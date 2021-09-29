fcd2ef2f6ab3712d26bcdb17c879f8cce469dbd5
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2021-09-29 14:07:37 +0530

    feat: basic directory structure setup
    Signed-off-by: Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ButtonAppBar from './components/ButtonAppBar/ButtonAppBar';
+import { Drawer } from '@mui/material';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Drawer
+        open={false}
+      ></Drawer>
+      <ButtonAppBar />
+      <Switch>
+        <Route path="/bill">bill</Route>
+        <Route path="/">collection</Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
