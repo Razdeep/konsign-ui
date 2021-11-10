e9cff2f2f8ed0d86a62d076c31583ecc67794c7e
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2021-11-10 13:31:45 +0530

    fix: code cleaned up
    Signed-off-by: Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ButtonAppBar from './components/ButtonAppBar/ButtonAppBar';
-import { Drawer } from '@mui/material';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   return (
-    // <BrowserRouter>
-    //   <Drawer
-    //     open={false}
-    //   ></Drawer>
-    //   <ButtonAppBar />
-    //   <Switch>
-    //     <Route path="/bill">bill</Route>
-    //     <Route path="/">collection</Route>
-    //   </Switch>
-    // </BrowserRouter>
-    <Dashboard></Dashboard>
+    <BrowserRouter>
+      <Dashboard></Dashboard>
+    </BrowserRouter>
   );
 }
 
