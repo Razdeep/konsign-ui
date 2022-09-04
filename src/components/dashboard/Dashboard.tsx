08c3494fbb54b694e47db56dc8473a4f295a00db
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2022-09-04 14:54:02 +0530

    feat: added icon in dashboard title bar
    Signed-off-by: Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -112,6 +112,7 @@ function DashboardContent() {
             >
               <MenuIcon />
             </IconButton>
+            <img src="konsign_icon.png" alt="logo"></img>
             <Typography
               component="h1"
               variant="h6"
