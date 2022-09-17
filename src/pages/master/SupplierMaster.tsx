8ef9af0c48b3d650cf2faab428a544b41454871b
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2022-09-17 19:51:05 +0530

    feat: SupplierMaster improved

--- src/pages/master/SupplierMaster.tsx
+++ src/pages/master/SupplierMaster.tsx
@@ -3,12 +3,22 @@ import Config from '../../util/config';
 import { useState } from 'react';
 import { Alert, Button, Snackbar, Table, TableCell, TableHead, TableRow } from '@mui/material';
 import { useAuth } from '../../util/auth';
+import { Refresh } from '@mui/icons-material';
 
 const SupplierMaster: React.FC<React.ReactNode> = () => {
 
+    class Supplier {
+        supplierId: string = '';
+        supplierName: string = '';
+    }
+
+    interface Master {
+        suppliers: Supplier[];
+    }
+
     const [errorMessage, setErrorMessage] = useState<String>('')
     const [errorVisibility, setErrorVisibility] = useState<boolean>(false)
-    const [suppliers, setSuppliers] = useState<String[] | null>(null)
+    const [suppliers, setSuppliers] = useState<Supplier[]>([])
     const auth = useAuth();
 
     const fetchSupplier = async () => {
@@ -18,38 +28,36 @@ const SupplierMaster: React.FC<React.ReactNode> = () => {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + auth?.user?.jwt
             }),
+            json: true
         };
 
         const response = await fetch(Config.GET_ALL_SUPPLIERS, requestOptions).catch(e => {
-                setErrorMessage('Something went wrong while trying to fetch the suppliers')
-                return
-            })
-        if (response?.status !== 200) {
+            setErrorMessage('Something went wrong while trying to fetch the suppliers')
+            return
+        })
+
+        if (response == null || response?.status !== 200) {
             setErrorMessage('Something went wrong while trying to fetch the suppliers')
             return
         }
         
-        const responseJsonStr = await response.text()
-        const responseJson = await JSON.parse(responseJsonStr)
-        const refreshedSuppliers: String[] = []
-        responseJson.suppliers.map((supplier: { [x: string]: String; }) => {
-            refreshedSuppliers.push(supplier['supplierName'])
-            return null
-        })
-        setSuppliers(refreshedSuppliers)
+        const master: Master = JSON.parse(await response?.text())
+        setSuppliers(master?.suppliers)
     }
 
     return (
         <>
-            <Button onClick={fetchSupplier}>Refresh</Button>
+            <Button onClick={fetchSupplier}><Refresh/>Refresh</Button>
             <Table>
-                <TableRow>
-                    <TableHead>Supplier name</TableHead>
-                </TableRow>
+                <TableHead>
+                    <TableCell>Supplier ID</TableCell>
+                    <TableCell>Supplier name</TableCell>
+                </TableHead>
                 {
-                    suppliers?.map(inp => (
+                    suppliers?.map((supplier: Supplier) => (
                         <TableRow>
-                            <TableCell>{inp}</TableCell>
+                            <TableCell>{supplier.supplierId}</TableCell>
+                            <TableCell>{supplier.supplierName}</TableCell>
                         </TableRow>
                     ))
                 }
