17dfc3350bf274e58e01a89ab383ab6cdbc66da4
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2022-09-21 07:55:52 +0530

    fix: config refactored

--- src/util/config.ts
+++ src/util/config.ts
@@ -1,24 +1,10 @@
-// Configure this variable
-const active_profile: string = 'dev';
-
-class LocalConfig {
-    static BACKEND_BASE_URL: string = 'http://localhost:8080/';
+class Config {
+    static BACKEND_BASE_URL: string = process.env.BASE_URL || 'https://konsign-api.herokuapp.com/';
     static BILL_ENTRY_URL = this.BACKEND_BASE_URL + 'billentry';
     static LOGIN_URL = this.BACKEND_BASE_URL + 'authenticate';
     static GET_ALL_SUPPLIERS: string = this.BACKEND_BASE_URL + 'suppliers';
     static DELETE_SUPPLIER: string = this.BACKEND_BASE_URL + 'supplier';
     static ADD_SUPPLIER: string = this.BACKEND_BASE_URL + 'addSupplier';
 };
-
-class DevConfig {
-    static BACKEND_BASE_URL: string = 'https://konsign-api.herokuapp.com/';
-    static BILL_ENTRY_URL = this.BACKEND_BASE_URL + 'billentry';
-    static LOGIN_URL = this.BACKEND_BASE_URL + 'authenticate';
-    static GET_ALL_SUPPLIERS: string = this.BACKEND_BASE_URL + 'suppliers';
-    static DELETE_SUPPLIER: string = this.BACKEND_BASE_URL + 'supplier';
-    static ADD_SUPPLIER: string = this.BACKEND_BASE_URL + 'addSupplier';
-};
-
-const Config = active_profile === 'dev' ? DevConfig : LocalConfig;
 
 export default Config;
