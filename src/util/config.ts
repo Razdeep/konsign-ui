da6a53b202729f1e08348dbd028ac9a5f06a666f
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2022-09-21 11:02:28 +0530

    fix: improved config with templatized strings
    Signed-off-by: Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>

--- src/util/config.ts
+++ src/util/config.ts
@@ -1,13 +1,13 @@
 class Config {
-    static BACKEND_BASE_URL: string = process.env.BASE_URL || 'https://konsign-api.herokuapp.com/';
-    static BILL_ENTRY_URL = this.BACKEND_BASE_URL + 'billentry';
-    static LOGIN_URL = this.BACKEND_BASE_URL + 'authenticate';
-    static GET_ALL_SUPPLIERS: string = this.BACKEND_BASE_URL + 'suppliers';
-    static DELETE_SUPPLIER: string = this.BACKEND_BASE_URL + 'supplier';
-    static ADD_SUPPLIER: string = this.BACKEND_BASE_URL + 'addSupplier';
-    static GET_ALL_BUYERS: string = `${this.BACKEND_BASE_URL}buyers`;
-    static ADD_BUYER: string = `${this.BACKEND_BASE_URL}addBuyer`;
-    static DELETE_BUYER: string = `${this.BACKEND_BASE_URL}buyer`;
+    static BACKEND_BASE_URL: string = process.env.BASE_URL || 'https://konsign-api.herokuapp.com';
+    static BILL_ENTRY_URL = `${this.BACKEND_BASE_URL}/billentry`;
+    static LOGIN_URL = `${this.BACKEND_BASE_URL}/authenticate`;
+    static GET_ALL_SUPPLIERS: string = `${this.BACKEND_BASE_URL}/suppliers`;
+    static DELETE_SUPPLIER: string = `${this.BACKEND_BASE_URL}/supplier`;
+    static ADD_SUPPLIER: string = `${this.BACKEND_BASE_URL}/addSupplier`;
+    static GET_ALL_BUYERS: string = `${this.BACKEND_BASE_URL}/buyers`;
+    static ADD_BUYER: string = `${this.BACKEND_BASE_URL}/addBuyer`;
+    static DELETE_BUYER: string = `${this.BACKEND_BASE_URL}/buyer`;
 };
 
 export default Config;
