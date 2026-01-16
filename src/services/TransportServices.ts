783daef2dc95b1f084fa802299e34bb66b7d870a
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2026-01-16 21:47:47 +0530

    feat: using ToastService in transport master
    Signed-off-by: Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>

--- src/services/TransportServices.ts
+++ src/services/TransportServices.ts
@@ -1,10 +1,7 @@
 import type Transport from '../model/Transport'
 import Config from '../util/config'
 import type KonsignResponse from '../model/KonsignResponse'
-
-interface Master {
-  data: Transport[]
-}
+import { ToastService } from './toast.service'
 
 export const fetchAllTransportsFromApi = async (auth: any): Promise<Transport[] | null> => {
   const requestOptions = {
@@ -16,30 +13,28 @@ export const fetchAllTransportsFromApi = async (auth: any): Promise<Transport[] 
     json: true,
   }
 
-  const response = await fetch(Config.TRANSPORTS_ENDPOINT, requestOptions).catch(() => {
+  const response = await fetch(Config.TRANSPORTS_ENDPOINT, requestOptions).catch((err) => {
+    console.error(err)
+    ToastService.error(err.message)
     return null
   })
 
   if (response == null || response?.status !== 200) {
+    ToastService.error('error fetching all transport')
     return null
   }
 
   try {
-    const master: Master = JSON.parse(await response?.text())
-    return master?.data
+    const res: KonsignResponse<Transport[]> = await response.json()
+    ToastService.success('successfully fetched all transports')
+    return res?.data
   } catch (e) {
     console.log(e)
     return []
   }
 }
 
-export const addTransport = async (
-  transport: Transport,
-  auth: any,
-  // remove below args somehow
-  setSnackbarMessage: any,
-  setSnackbarVisibility: any,
-) => {
+export const addTransport = async (transport: Transport, auth: any): Promise<void> => {
   const serializedData = JSON.stringify(transport)
   const requestOptions = {
     method: 'POST',
@@ -51,22 +46,21 @@ export const addTransport = async (
     json: true,
   }
 
-  const response = await fetch(Config.TRANSPORTS_ENDPOINT, requestOptions)
+  const response = await fetch(Config.TRANSPORTS_ENDPOINT, requestOptions).catch((err) => {
+    console.error(err)
+    ToastService.error(err.message)
+    return null
+  })
 
-  if (response.status !== 200) {
-    setSnackbarMessage('Failed to add transport')
-    setSnackbarVisibility(1)
+  if (response === null || response.status !== 200) {
+    ToastService.error(`Failed saving transport ${transport.transportName}`)
     return
   }
 
-  setSnackbarMessage((await response?.json())?.message)
-  setSnackbarVisibility(2)
+  ToastService.success(`Successfully saved ${transport.transportName}`)
 }
 
-export const deleteTransportFromApi = async (
-  transport: String,
-  auth: any,
-): Promise<KonsignResponse> => {
+export const deleteTransportFromApi = async (transport: String, auth: any): Promise<void> => {
   const requestOptions = {
     method: 'DELETE',
     headers: new Headers({
@@ -77,16 +71,16 @@ export const deleteTransportFromApi = async (
   }
 
   const response = await fetch(`${Config.TRANSPORTS_ENDPOINT}/${transport}`, requestOptions).catch(
-    (e) => {
-      throw e
+    (err) => {
+      console.error(err)
+      ToastService.error(err)
+      return null
     },
   )
 
-  if (response.status !== 200) {
-    const errorMessage = 'Something went wrong while deleting transport'
-    console.error(errorMessage)
-    throw new Error(errorMessage)
+  if (response === null || response?.status !== 200) {
+    ToastService.error(`failed deleting transport ${transport}`)
   }
 
-  return response.json()
+  ToastService.success(`Successfully deleted transport ${transport}`)
 }
